3c26c42c2f0e2ff45faa2b445e475125f6a59d22
LeventSoykan <levent.soykan@turkcell.com.tr>
2022-11-04 21:06:23 +0300

    Add package imports

--- __init__.py
+++ __init__.py
@@ -3,3 +3,5 @@ Levent Tools for Python
 ------------------------
 leventools is a package for miscellaneous tools for data science and python scripts
 """
+from leventools import data_science
+from leventools import tools
